8c893144d320c183b8f5f6369941dbf92ef295a9
Lusmori <thanhtungnguyennnn@gmail.com>
2025-11-17 21:52:16 +0700

    Skill classification

--- Assets/Scripts/Skill/SkillManager.cs
+++ Assets/Scripts/Skill/SkillManager.cs
@@ -1,16 +1,36 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SkillManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private string skill1 = "Record Trap";
+    private string skill2 = "Shoes";
+    private string skill3 = "Shield";
 
-    // Update is called once per frame
-    void Update()
+    public string currentSkill; //Lưu kĩ năng hiện tại
+    private bool skillSelected = false;
+    public void SelecterSkill(string skillName)
     {
-        
+        if(skillSelected)
+        {
+            return;
+        }
+        currentSkill = skillName;
+        Debug.Log("Kĩ năng hiện tại: " + currentSkill);
+        skillSelected = true;
+    }
+    public void CheckTrap()
+    {
+        if(currentSkill == skill1)
+        {
+            Debug.Log("Record lại 5s xem trap");
+        }
+        else if(currentSkill == skill2)
+        {
+            Debug.Log("Kích hoạt giày thép gai");
+        }
+        else if(currentSkill == skill3)
+        {
+            Debug.Log("Kích hoạt khiên");
+        }
     }
 }
