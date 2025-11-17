98edaafb65b371a24f720639ced2f73a10dd01c7
NguyenTruongTrongHieu <tronghieutruong5102004@gmail.com>
2025-11-17 18:28:03 +0700

    Add check trap and player skill scripts

---
+++ Assets/Scripts/PLayer/PlayerSkill.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class PlayerSkill : MonoBehaviour
+{
+    public bool shieldActive = false;//skill khiên càn vật rơi
+    public bool shoesActive = false;//skill giày thép gai
+}
