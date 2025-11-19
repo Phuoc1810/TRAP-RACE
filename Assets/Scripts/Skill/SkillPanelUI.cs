9e255309f465ae6ac1e77489d7a27005f41a617f
Lusmori <thanhtungnguyennnn@gmail.com>
2025-11-19 16:23:30 +0700

    Tutorials Text For Skill Choosing

--- Assets/Scripts/Skill/SkillPanelUI.cs
+++ Assets/Scripts/Skill/SkillPanelUI.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkillPanelUI : MonoBehaviour
 {
@@ -9,27 +12,52 @@ public class SkillPanelUI : MonoBehaviour
     private Vector2 hiddenPos; //Vị trí dưới màn hình
     private Vector2 shownPos; //Vị trí chính giữa màn hình
 
+    [SerializeField] private GameObject skillInforPanel;
+    [SerializeField] private Text textInfor;
+
+    [Header("Text Setting")]
+    private float minAlpha = 0.3f;
+    private float maxAlpha = 1f;
+    private float speedAlpha = 0.5f;
+    private float alphaValue;
+
     private void Start()
     {
         hiddenPos =new Vector2(0, -582);
         shownPos = new Vector2(0, 0);
-        //skillPanel.gameObject.SetActive(false);
+    }
+    private void Update()
+    {
+        alphaValue = Mathf.PingPong(Time.time * speedAlpha, maxAlpha - minAlpha) + minAlpha;
+        ChangeAlphaValueForText();
     }
     public void ShowPanel()
     {
         skillPanel.gameObject.SetActive(true);
         Vector2 targetPos = Vector2.Lerp(skillPanel.anchoredPosition, shownPos, speedPosition);
         skillPanel.anchoredPosition = targetPos;
+        StartCoroutine(ShowInfor());
     }
     public void HidePanel()
     {
         Vector2 targetPos = Vector2.Lerp(skillPanel.anchoredPosition, hiddenPos, speedPosition);
         skillPanel.anchoredPosition = targetPos;
-       // StartCoroutine(SetActive());
+       StartCoroutine(HideInfor());
     }
-    private IEnumerator SetActive()
+    private void ChangeAlphaValueForText()
+    {
+        Color color = textInfor.color;
+        color.a = alphaValue;
+        textInfor.color = color;
+    }
+    private IEnumerator ShowInfor()
     {
         yield return new WaitForSeconds(1.5f);
-        skillPanel.gameObject.SetActive(false);
+        skillInforPanel.SetActive(true);
+    }
+    private IEnumerator HideInfor()
+    {
+        yield return new WaitForSeconds(1.5f);
+        skillInforPanel.SetActive(false);
     }
 }
